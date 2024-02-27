cc7781c9583acb8e00e2dd9e9c97237bf613ca27
cviegas <cesairepro@gmail.com>
2024-02-27 20:20:31 +0100

    squid

--- includes/minisquidx/srcs/draw/screen.c
+++ includes/minisquidx/srcs/draw/screen.c
@@ -6,7 +6,7 @@
 /*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 18:28:32 by cviegas           #+#    #+#             */
-/*   Updated: 2024/02/27 18:02:38 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/02/27 18:07:07 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,7 @@ void	draw_color(t_color color, t_v2i pos, t_game *g)
 		+ pos[0]] = color.integer;
 }
 
-void	print_pixel(t_color color, t_v2i pos, t_game *g)
-{
-	mlx_pixel_put(g->mlx, g->mlx_win, pos[0], pos[1], color.integer);
-}
-
 void	print_screen(t_game *g)
 {
-	// t_v2i	pos;
 	mlx_put_image_to_window(g->mlx, g->mlx_win, g->screen->img, 0, 0);
-	// pos[1] = 0;
-	// while (pos[1] < W_HEIGHT)
-	// {
-	// 	pos[0] = 0;
-	// 	while (pos[0] < W_WIDTH)
-	// 	{
-	// 		print_pixel(get_color(*g->screen, pos), pos, g);
-	// 		pos[0]++;
-	// 	}
-	// 	pos[1]++;
-	// }
 }
